5ab2f43eee6ea2a416b2927afb7df59e13725ebb
TAMIOOOOO <tamioalan2005@gmail.com>
2025-04-22 18:29:03 +0800

    Deleted back end, Data Creation, side bar Final, Images

---
+++ data/pokeNames.js
@@ -0,0 +1,33 @@
+const pokeNames = [{
+    name: 'Uno',
+    image: 'images/1.png',
+    baliwRating: '3 stars'
+}, {
+    name: 'Dos',
+    image: 'images/1.png',
+    baliwRating: '3 stars'
+}, {
+    name: 'Tres',
+    image: 'images/1.png',
+    baliwRating: '3 stars'
+}, {
+    name: 'Quatro',
+    image: 'images/1.png',
+    baliwRating: '3 stars'
+}, {
+    name: 'Singko',
+    image: 'images/1.png',
+    baliwRating: '3 stars'
+}, {
+    name: 'Sais',
+    image: 'images/1.png',
+    baliwRating: '3 stars'
+}, {
+    name: 'Syete',
+    image: 'images/1.png',
+    baliwRating: '3 stars'
+}, {
+    name: 'Otso',
+    image: 'images/1.png',
+    baliwRating: '3 stars'
+}]
---
+++ scripts/output.js
@@ -0,0 +1,7 @@
+let pokenamesHtml = '';
+
+pokeNames.forEach((pokeNames) => {
+    pokenamesHtml += `
+    
+    `;
+});
