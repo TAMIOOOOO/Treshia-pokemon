044f400593247d34a5eb1569d4a68f7d23c2dbf3
TAMIOOOOO <tamioalan2005@gmail.com>
2025-04-22 18:32:30 +0800

    sidebar contents completion

--- scripts/output.js
+++ scripts/output.js
@@ -2,6 +2,14 @@ let pokenamesHtml = '';
 
 pokeNames.forEach((pokeNames) => {
     pokenamesHtml += `
-    
+    <div class="pokemon-sidebar-names-container">
+        <div class="pokemon-sidebar-image-container">
+          <img class="pokemonImage" src="${pokeNames.image}" alt="" />
+        </div>
+
+        <div class="pokemon-sidebar-name">${pokeNames.name}</div>
+      </div>
     `;
 });
+
+document.querySelector('.pokemon-sidebar-container').innerHTML = pokenamesHtml;
