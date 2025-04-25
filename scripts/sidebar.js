a222780855ac82666b8a71b2b0e0fb53be7ae77f
TAMIOOOOO <tamioalan2005@gmail.com>
2025-04-25 23:08:04 +0800

    finally got it working

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -2,16 +2,37 @@ let sidebarHTML = '';
 
 pokeNames.forEach((pokeNames) => {
     sidebarHTML += `
-    <button> <img src="${pokeNames.image}" alt="">${pokeNames.name}</button>
+    <button class="js-opening-button" onclick="clicked(this)" 
+    data-pokeNames-id="${pokeNames.id}"> 
+    <img src="${pokeNames.image}" alt="">
+    ${pokeNames.name}
+    </button>
     `;
 });
-
 document.querySelector('.contents').innerHTML = sidebarHTML;
 
-let mainCard = '';
 
-pokeNames.forEach((pokeNames) => {
-    mainCard += `
+
+function clicked(button) {
+    const id = button.getAttribute('data-pokeNames-id');
+    const found = pokeNames.find(pokeNames => pokeNames.id === id);
+
+    if (found) {
+        document.querySelector('.card-container').innerHTML = `
+        <div class="card-inner">
+            <div class="card-front"><img src="${found.image}" alt="" /></div>
+            <div class="card-back">
+              <img class="back-image" src="${found.image}" alt="">
+              <div class="names-baliwRating">
+                <h3>Name: ${found.name}</h3>
+                <h3>Baliw Rating: ${found.baliwRating}</h3>
+              </div>
+              </div>
+            </div>
+          </div>
+      `;
+    } else {
+        document.querySelector('.card-inner').innerHTML = `<p>Pokemon not found.</p>`;
+    }
     
-    `;
-});
+}
